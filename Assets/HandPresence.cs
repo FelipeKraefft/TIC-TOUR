8ad9917cf5411fc28967b7d899644ba4de4f32c4
FelipeKraefft <101569052+FelipeKraefft@users.noreply.github.com>
2022-06-24 16:08:39 -0300

    HandsPresence (without animation)

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    private InputDevice targetDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCaracteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCaracteristics,devices);
+
+        InputDeviceCharacteristics leftControllerCaracteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCaracteristics, devices);
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+
+
+    }
+}
