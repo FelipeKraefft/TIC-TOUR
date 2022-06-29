a9c568e8ac0cebf42afc24c145cd92dbd130420b
FelipeKraefft <101569052+FelipeKraefft@users.noreply.github.com>
2022-06-28 22:58:32 -0300

    Hands Animations

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -5,34 +5,69 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCaracteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+
     private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
-        InputDeviceCharacteristics rightControllerCaracteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCaracteristics,devices);
-
-        InputDeviceCharacteristics leftControllerCaracteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(leftControllerCaracteristics, devices);
+        
+        InputDevices.GetDevicesWithCharacteristics(controllerCaracteristics, devices);
 
         if(devices.Count > 0)
         {
             targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(Controller => Controller.name == targetDevice.name);
+            if(prefab){
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else{
+                Debug.LogError("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab,transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }   
+
+    }
+
+    void UpdateHandAnimation(){
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue)){
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else{
+            handAnimator.SetFloat("Trigger", 0);
         }
 
+        if(targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue)){
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else{
+            handAnimator.SetFloat("Grip", 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-
-        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-
-        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-
-
+        if(showController){
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else{
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+            UpdateHandAnimation();
+        }
     }
 }
