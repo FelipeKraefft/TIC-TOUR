a31b41ed8a7a3c0ab84a463e5320ad4bf950fae5
FelipeKraefft <101569052+FelipeKraefft@users.noreply.github.com>
2022-09-14 10:59:24 -0300

    added graivity for fire exytinguishers

---
+++ Assets/Scripts/AddGravityWhenGrab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGravityWhenGrab : MonoBehaviour
+{
+    GameObject grabbedObject;
+    public Collider rightHandCollider;
+    public Collider leftHandCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other == rightHandCollider || other == leftHandCollider)
+        {
+            grabbedObject = gameObject;
+            grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
